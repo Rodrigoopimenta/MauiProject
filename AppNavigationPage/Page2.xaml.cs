78d8ae802e6d061958ef23c1f34272452e4a1564
rodrigoopimenta <rodrigo.pimenta@xnetweb.com.br>
2025-05-24 12:04:03 -0300

    Add project files.

---
+++ AppNavigationPage/Page2.xaml.cs
@@ -0,0 +1,14 @@
+namespace AppNavigationPage;
+
+public partial class Page2 : ContentPage
+{
+	public Page2()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new Page3());
+    }
+}
